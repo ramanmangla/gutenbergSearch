b4abe5ed0ad98dceeb45f6e20d921bf05cdbf8ef
Raman Mangla <ramanmangla7@gmail.com>
2019-08-06 12:18:35 -0400

    Added Elasticsearch schema and added 100 books from the Gutenberg database for indexing

--- server/connection.js
+++ server/connection.js
@@ -31,3 +31,27 @@ async function resetIndex() {
   await client.indices.create({ index });
   await putBookMapping();
 }
+
+/** Add book section schema mapping to ES */
+async function putBookMapping() {
+  const schema = {
+    title: { type: 'keyword' },
+    author: { type: 'keyword' },
+    location: { type: 'integer' },
+    text: { type: 'text' }
+  };
+
+  return client.indices.putMapping({
+    index,
+    type,
+    body: { properties: schema }
+  });
+}
+
+module.exports = {
+  client,
+  index,
+  type,
+  checkConnection,
+  resetIndex
+};
